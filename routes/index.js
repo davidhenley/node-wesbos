0ed02b999bc0071a43b9213a0be594bec6c5d983
David Henley <henleydr@gmail.com>
2017-06-06 22:57:30 -0500

    Templating

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  res.render('hello', {
+    name: 'David',
+    dog: req.query.dog
+  });
 });
 
 router.get('/reverse/:name', (req, res) => {
