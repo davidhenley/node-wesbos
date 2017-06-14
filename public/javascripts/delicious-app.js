f30acad8549845a7a1d3c8a92712bf0f6f677a8f
David Henley <henleydr@gmail.com>
2017-06-14 15:53:01 -0500

    Adding hearts

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -4,9 +4,13 @@ import { $, $$ } from './modules/bling';
 import autocomplete from './modules/autocomplete';
 import typeAhead from './modules/typeAhead';
 import map from './modules/map';
+import heart from './modules/heart';
 
 autocomplete($('#address'), $('#lat'), $('#lng'));
 
 typeAhead( $('.search') );
 
 map( $('#map') );
+
+const heartForms = $$('form.heart');
+heartForms.on('submit', heart);
